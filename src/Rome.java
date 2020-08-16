e8adb35d5bdba6618b3b4326e4d410a552b7985f
alekseyusenko <69609627+alekseyusenko@users.noreply.github.com>
2020-08-17 01:14:18 +0300

    Update Rome.java

--- src/Rome.java
+++ src/Rome.java
@@ -4,7 +4,7 @@ public class Rome {
 
 
 
-    public int convert(String[] sArr, int x) {
+    public int romanToIntConverter(String[] sArr, int x) {
 
         int num = 0;
 
@@ -30,6 +30,75 @@ public class Rome {
             else result = false;
         }
         return result;
+    }
+
+    private StringBuilder intUnitsToRome (int units) {
+        StringBuilder unitsResult = new StringBuilder();
+        if (units > 0) {
+
+            if (units <= 3) {
+                for (int j = 0; j < units; j++)
+                    unitsResult.append("I");
+            } else if (units == 4) {
+                unitsResult.append("IV");
+            } else if ((units > 4) & (units < 9)) {
+                unitsResult.append("V");
+                for (int j = 0; j < units - 5; j++)
+                    unitsResult.append("I");
+            } else if (units == 9) unitsResult.append("IX");
+        }
+
+        else if (units < 0) {
+            unitsResult.append("Скорее всего, римляне не использовали отрицательные числа. Но если бы использовали, результат ваших вычислений был бы таким: ");
+            unitsResult.append("-");
+
+            if (units >= -3) {
+                for (int j = 0; j > units; j--)
+                    unitsResult.append("I");
+            } else if (units == -4) {
+                unitsResult.append("IV");
+            } else if ((units < -4) & (units > -9)) {
+                unitsResult.append("V");
+                for (int j = 0; j > units + 5; j--)
+                    unitsResult.append("I");
+            } else unitsResult.append("IX");
+        }
+
+
+            return unitsResult;
+
+    }
+
+    public String intToRomeConverter(int calcResult) {
+
+        StringBuilder romeResult = new StringBuilder();
+        if ((calcResult < 10)) romeResult.append(intUnitsToRome(calcResult));
+
+        else if ( (calcResult / 10 != 0) & (calcResult < 100) ) {
+            int dozens = calcResult / 10;
+            int units = calcResult % 10;
+
+            if (dozens <= 3) {
+                for (int j = 0; j < dozens; j++)
+                    romeResult.append("X");
+            } else if (dozens == 4) {
+                romeResult.append("XL");
+            } else if ((dozens > 4) & (dozens < 9)) {
+                romeResult.append("L");
+                for (int j = 0; j < dozens - 5; j++)
+                    romeResult.append("X");
+            } else if (dozens == 9) romeResult.append("XC");
+
+            romeResult.append(intUnitsToRome(units));
+
+        }
+        else if (calcResult == 100) romeResult.append("C");
+
+        else if (calcResult == 0) romeResult.append("N");
+
+
+
+        return romeResult.toString();
     }
 }
 
